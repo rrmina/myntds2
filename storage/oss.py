da5332dc2c8912cf80e8ecd8884ad654d2f26323
Rusty John Lloyd Mina <rustyjohnlloyd.mina@GFI05757.local>
2024-09-30 02:19:16 +0800

    Added OSS read parquet from odps method

--- storage/oss.py
+++ storage/oss.py
@@ -3,7 +3,8 @@ import oss2
 import pandas as pd
 from io import BytesIO
 from dotenv import load_dotenv
-    
+from ..sql.odps import SimpleODPSClient
+from typing import List
 class SimpleOSSClient:
     def __init__(self,
         access_id_key: str = 'ODPS_ID',
@@ -19,9 +20,11 @@ class SimpleOSSClient:
         
         auth = oss2.Auth(access_key_id=access_id, access_key_secret=secret_access_key)
         bucket = oss2.Bucket(auth=auth, endpoint=endpoint, bucket_name=bucket_name)
-        
+        o = SimpleODPSClient()
+
         self.bucket = bucket
-    
+        self.o = o.o
+
     def write_feather(self,
         key: str,
         df: pd.DataFrame
@@ -66,3 +69,46 @@ class SimpleOSSClient:
         
         return df
     
+    def list_parquet_paths_from_odps(self,
+        project: str = 'mynt_ds_dev',
+        table_name: str = None,
+        partition: str = None
+    ) -> List[str]:
+        
+        t = self.o.get_table(name=table_name, project=project)
+        parts = [key for key in t.location.split("/")[4:] if len(key) > 0]
+        parts.append(partition)
+        prefix = "/".join(parts)
+
+        paths = [
+            obj.key for obj in oss2.ObjectIteratorV2(self.bucket, prefix=prefix)
+            if not obj.key.endswith(".meta")
+        ]
+
+        return paths
+
+    def read_parquet_from_odps(self,
+        project: str = 'mynt_ds_dev',
+        table_name: str = None,
+        partition: str = None
+    ) -> pd.DataFrame:
+
+        parquet_list = self.list_parquet_paths_from_odps(
+            table_name = table_name,
+            partition = partition,
+            project = project,
+        )
+        
+        if len(parquet_list) == 0:
+            print("Parquet Empty. Exiting now.")
+            return 0
+        
+        for i in range(len(parquet_list)):
+            parquet_path = parquet_list[i]
+            temp = self.read_parquet(parquet_path)
+            if i == 0:
+                df = temp
+            else:
+                df = pd.concat([df, temp])
+
+        return df
